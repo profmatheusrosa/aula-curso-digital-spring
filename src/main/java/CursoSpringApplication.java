6d0fff39f9ac79b00102c1d760e12d82c6ff8970
Matheus Rosa <matheus_luis@yahoo.com.br>
2026-03-22 06:14:36 -0300

    feat: Initialize Spring Boot project with main application class, test, and hello world example.

--- src/main/java/CursoSpringApplication.java
+++ src/main/java/CursoSpringApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,4 +11,3 @@ public class CursoSpringApplication {
     }
 
 }
-
--- src/test/java/CursoSpringApplicationTests.java
+++ src/test/java/CursoSpringApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CursoSpringApplicationTests {
